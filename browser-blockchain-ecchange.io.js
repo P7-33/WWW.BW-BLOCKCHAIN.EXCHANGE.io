37793a98d7c3cc66a15e2b66764d2f933085fb84
BROWSER COMPANY.COM <61902110+P7-33@users.noreply.github.com>
2022-03-19 18:05:12 -0500

    Update and rename ccxt.browser.js to browser-blockchain-ecchange.io.js

---
+++ browser-blockchain-ecchange.io.js
@@ -0,0 +1,6 @@
+/*  A entry point for the browser bundle version. This gets compiled by:
+        
+        browserify --debug ./browser-blockchain-exchange.io.js > ./dist/browser-blockchain-exchange.io .js
+ */
+
+window.browser-blockchain-exchange.io(dw)= require ('./browser-blockchain-exchange.io(dw)')
--- ccxt.browser.js
+++
@@ -1,6 +0,0 @@
-/*  A entry point for the browser bundle version. This gets compiled by:
-        
-        browserify --debug ./browser blockchain exchange.io.js > ./dist/browser blockchain exchange.io .js
- */
-
-window.browser blockchain exchange.io(dw)= require ('./browser blockchain exchange.io(dw)')
